f963933db6da828a6b04cf08fcf1f2971ed75e1d
youngancient <tochyokoye@gmail.com>
2025-01-20 00:03:39 +0100

    worked on shamir

--- shamir/src/main.rs
+++ shamir/src/main.rs
@@ -1,9 +1,3 @@
-use shamir::shamir;
-
-fn main() {
-    let secret = 340282366920938463463374607431768211455;
-    let threshold = 4;
-    let shares_no = 10;
-    let shares = shamir::share_secret(secret, threshold, shares_no);
-    println!("{:?}", shares);
+fn main(){
+    todo!();
 }
