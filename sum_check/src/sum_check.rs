e660979ff9d56382f61293acb08dfed6a5eae196
youngancient <tochyokoye@gmail.com>
2025-02-01 14:10:18 +0100

    mod: completed sum_check with tests

--- sum_check/src/sum_check.rs
+++ sum_check/src/sum_check.rs
@@ -54,60 +54,61 @@ pub fn prove<F: PrimeField>(polynomial: &mut EvaluationForm<F>, claim_sum: F) ->
     let mut transcript: Transcript<F, Keccak256> = Transcript::init(Keccak256::new());
     transcript.append(&EvaluationForm::to_bytes(&polynomial.eval_form));
 
-    transcript.append(
-        claim_sum
-            .into_bigint()
-            .to_bytes_be()
-            .as_slice(),
-    );
+    // let random_array = [F::from(5), F::from(10), F::from(2)]; // replace with transcript
+
+    transcript.append(claim_sum.into_bigint().to_bytes_be().as_slice());
 
     let mut proof: Proof<F> = Proof {
         sum: F::zero(),
         polynomials: Vec::new(),
     };
-
     proof.sum = claim_sum;
-    for i in 0..polynomial.number_of_variables - 1 {
-        // println!("i -> {}",i);
-        let univariate_poly_eval_form =
-            evaluate_at_two_vars(&polynomial.eval_form, (i + 1) as usize);
-        println!("prover poly -> {:?}",univariate_poly_eval_form);
-        transcript.append(&EvaluationForm::to_bytes(&univariate_poly_eval_form));
-        // println!("poly -> {:?}", univariate_poly_eval_form);
-        proof.polynomials.push(univariate_poly_eval_form);
+    for i in 1..=polynomial.number_of_variables {
+        // transcript.append();
+        let challenge = transcript.hash();
 
-        let random_challenge = transcript.hash();
-        println!("prover random chall-> {:?}", random_challenge);
-        polynomial.partial_evaluate(i + 1, random_challenge);
+        let univariate_poly = evaluate_at_two_vars(&polynomial.eval_form, 1 as usize);
+
+        transcript.append(&EvaluationForm::to_bytes(&univariate_poly));
+
+        proof.polynomials.push(univariate_poly);
+        // println!("sum -> {:?}", get_sum_at_0_and_1(&polynomial.eval_form));
+        polynomial.partial_evaluate(i, challenge);
+        // println!("poly -> {:?}", polynomial.eval_form);
     }
-    // println!("proof-> {:?}",proof);
     proof
 }
 
 pub fn verify<F: PrimeField>(proof: Proof<F>, polynomial: &mut EvaluationForm<F>) -> bool {
-    let mut claimed_sum = proof.sum;
     let mut transcript: Transcript<F, Keccak256> = Transcript::init(Keccak256::new());
     transcript.append(&EvaluationForm::to_bytes(&polynomial.eval_form));
-    transcript.append(claimed_sum.into_bigint().to_bytes_be().as_slice());
-    let mut random_challenges:Vec<F> = Vec::new();
-    for poly in proof.polynomials {
-        let derived_sum = get_sum_at_0_and_1(&poly);
-        println!("derived sum -> {}",derived_sum);
-        if claimed_sum != derived_sum {
+    transcript.append(proof.sum.into_bigint().to_bytes_be().as_slice());
+
+    // let random_array = [F::from(5), F::from(10), F::from(2)]; // replace with transcript
+    let mut claimed_sum = proof.sum;
+    let mut random_challenges: Vec<F> = Vec::new();
+    for univariate_poly in proof.polynomials {
+        let verified_sum = get_sum_at_0_and_1(&univariate_poly);
+
+        // checks if the sums are equal
+        if claimed_sum != verified_sum {
             return false;
         }
-        transcript.append(&EvaluationForm::to_bytes(&poly));
-        let random_challenge = transcript.hash();
-        random_challenges.push(random_challenge);
-        println!("verifier random chall-> {:?}", random_challenge);
-        claimed_sum = interpolate_and_evaluate((poly[0], poly[1]), random_challenge);
+        let challenge = transcript.hash();
+        // the sum for the next univariate_poly
+        claimed_sum = interpolate_and_evaluate((univariate_poly[0], univariate_poly[1]), challenge);
+        transcript.append(&EvaluationForm::to_bytes(&univariate_poly));
+
+        random_challenges.push(challenge);
     }
 
-    // Oracle check
-    if polynomial.evaluate(random_challenges) != claimed_sum{
+    let derived_sum = polynomial.evaluate(random_challenges);
+
+    // oracle check
+    if claimed_sum != derived_sum {
         return false;
     }
-    return true;
+    true
 }
 
 #[cfg(test)]
@@ -115,6 +116,7 @@ pub fn verify<F: PrimeField>(proof: Proof<F>, polynomial: &mut EvaluationForm<F>
 mod tests {
     use super::*;
     use ark_bn254::Fq;
+    use ark_ff::UniformRand;
 
     fn get_test_poly() -> EvaluationForm<Fq> {
         EvaluationForm::new(vec![
@@ -181,7 +183,7 @@ mod tests {
     #[test]
     fn test_prove2() {
         let mut poly2 = get_test_poly2();
-        let sum  = get_sum_at_0_and_1(&poly2.eval_form);
+        let sum = get_sum_at_0_and_1(&poly2.eval_form);
         let proof = prove(&mut poly2, sum);
         assert_eq!(proof.sum, Fq::from(12));
     }
@@ -190,17 +192,26 @@ mod tests {
     fn test_prove_and_verify_valid_proof() {
         let mut poly1 = get_test_poly();
         let sum = get_sum_at_0_and_1(&poly1.eval_form);
-        let proof = prove(&mut poly1, sum);
+        let proof = prove(&mut poly1.clone(), sum);
         let is_valid = verify(proof, &mut poly1);
         assert_eq!(is_valid, true);
     }
 
-    // #[test]
-    // fn test_prove_and_verify_valid_proof2() {
-    //     let mut poly2 = get_test_poly2();
-    //     let sum = get_sum_at_0_and_1(&poly2.eval_form);
-    //     let proof = prove(&mut poly2, sum);
-    //     let is_valid = verify(proof, &poly2);
-    //     assert_eq!(is_valid, true);
-    // }
+    #[test]
+    fn test_prove_and_verify_valid_proof2() {
+        let mut poly2 = get_test_poly2();
+        let sum = get_sum_at_0_and_1(&poly2.eval_form);
+        let proof = prove(&mut poly2.clone(), sum);
+        let is_valid = verify(proof, &mut poly2);
+        assert_eq!(is_valid, true);
+    }
+
+    #[test]
+    fn test_prove_and_verify_invalid_proof() {
+        let mut poly1 = get_test_poly();
+        let sum = Fq::from(100000); // guessed sum
+        let invalid_proof = prove(&mut poly1.clone(), sum); // invalid proof
+        let is_valid = verify(invalid_proof, &mut poly1);
+        assert_eq!(is_valid, false);
+    }
 }
